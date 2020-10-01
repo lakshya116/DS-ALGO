dd40aa402d593891b5fa3b1b357960d8133fec0b
lakshya116 <72179749+lakshya116@users.noreply.github.com>
2020-10-01 09:22:33 +0530

    Pattern Cpp
    This is a program to print pattern
    *
    **
    ***
    ****
    ........upto n as use input depend program

---
+++ Arrays/Pattern.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h> 
+#inlcude<iostream>
+using namespace std; 
+  
+ //pattern print code
+
+
+
+
+int main () 
+{ 
+int n;
+cin>>n;
+
+for(int i=1;i<n;i++){
+for(int j=0;j<i;j++){
+cout<<"*";
+}
+cout<<"\n";
+}
+
+  
+ return 0; 
+} 
